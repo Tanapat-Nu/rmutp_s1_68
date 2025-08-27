6dc10d53d7a59d8b8c32ea1ab1b2831df86297af
Tanapat Nunkhong <tanapat-nu@rmutp.ac.th>
2025-08-27 14:53:01 +0700

    1

--- app/dist/index.js
+++
@@ -1,5 +0,0 @@
-import { Hono } from "hono";
-const app = new Hono();
-app.get('/', (c) => c.text('Hono!'));
-app.get('/about', (c) => { return c.json({ message: "Tanapat Nunkhong " }); });
-export default app;
--- app/dist/server.js
+++
@@ -1,5 +0,0 @@
-import { serve } from "@hono/node-server";
-import app from "./index.js";
-serve(app, (info) => {
-    console.log(`Server is running at http://localhost:${info.port}`);
-});
