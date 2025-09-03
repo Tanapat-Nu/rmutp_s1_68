dbee2c97680d896ee3b221b0b18d8b0e4bcdf422
Tanapat Nunkhong <tanapat-nu@rmutp.ac.th>
2025-09-03 17:31:56 +0700

    save to db

--- app/index.ts
+++ app/index.ts
@@ -16,19 +16,33 @@ app.get("/profile", async (c) => {
 });
 app.post("/profile", async (c) => {
     //logic to create a new profile
-    const boby = await c.req.json();
-    console.log('input of profile', boby);
-    console.log('body.password(original)', boby.password);
+    const body = await c.req.json();
+    console.log('input of profile', body);
+    console.log('body.password(original)', body.password);
+
+    c.status(503);
+    return c.json({
+     message: "service unavailable for demo",
+     data: "server error"
+    })
+    
 
     //encode password
-    const passwordHash = await bcrypt.hash(boby.password,18);
+    const passwordHash = await bcrypt.hash(body.password,18);
     console.log('hash.password(after)',passwordHash);
+    body.password = passwordHash;
+    console.log('body.password(replace)',body);
+
 
     //save to db
-
+    body.status= false;
+    const result = await prisma.profile.create({
+        data:body
+    });
     // output
     return c.json({
-        message: "create profile completed"
+        message: "create profile completed",
+        data: result
     })
 });
 
