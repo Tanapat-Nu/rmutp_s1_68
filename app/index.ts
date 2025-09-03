178d3e90689a426de050d35dc2b1ec2fd74f5201
Tanapat Nunkhong <tanapat-nu@rmutp.ac.th>
2025-09-03 16:44:29 +0700

    install types/bcrypt

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,7 @@
 import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client'
+import * as bcrypt from "bcrypt";
+
 // import { PrismaClient } from "../generated/prisma/client";
 
 const prisma = new PrismaClient();
@@ -7,10 +9,27 @@ const prisma = new PrismaClient();
 const app = new Hono();
 app.get('/', (c) => c.text('Hono!'));
 app.get('/about', (c) => { return c.json({ message: "Tanapat Nunkhong " }) });
-app.get("/profile", async(c) => {
+app.get("/profile", async (c) => {
     // logic
     const profile = await prisma.profile.findMany();
     return c.json(profile);
+});
+app.post("/profile", async (c) => {
+    //logic to create a new profile
+    const boby = await c.req.json();
+    console.log('input of profile', boby);
+    console.log('body.password(original)', boby.password);
+
+    //encode password
+    const passwordHash = await bcrypt.hash(boby.password,18);
+    console.log('hash.password(after)',passwordHash);
+
+    //save to db
+
+    // output
+    return c.json({
+        message: "create profile completed"
+    })
 });
 
 export default app;
